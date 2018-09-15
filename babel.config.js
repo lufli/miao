3f42084a91bebc6e1b0be6609c0035ef9fd03bcc
Lufan Li <lufan@pulsedata.io>
2018-09-14 23:49:22 -0400

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
+/* eslint-disable */
 module.exports = {
-  presets: [
-    '@vue/app'
+  'presets': [
+    [
+      '@vue/app',
+      {
+        'useBuiltIns': 'entry'
+      }
+    ]
+  ],
+  'plugins': [
+    [
+      'transform-imports',
+      {
+        'vuetify': {
+          'transform': 'vuetify/es5/components/${member}',
+          'preventFullImport': true
+        }
+      }
+    ]
   ]
 }
